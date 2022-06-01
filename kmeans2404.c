985de7e266830528414c1ed4ad937aa539221832
dana <danac4@mail.tau.ac.il>
2022-06-01 19:39:38 +0300

    added comments to c code

--- kmeans2404.c
+++ kmeans2404.c
@@ -8,17 +8,25 @@
 
 static PyObject* fit(PyObject *self, PyObject *args);
 
+/* Cluster definition:
+count = number of vectors assigned to the cluster 
+newPoints = sum of all vectors assigned to the cluster, instead of matrix of all vectors
+centroid = current centroid vector
+ */
 typedef struct{ 
     int count;
     double* newPoints;
     double* centroid;
 } Cluster;
 
+/* Point definition:
+vector = the point's data vector
+*/
 typedef struct{
     double* vector;
 } Point;
 
-
+/* Frees points memory, used before returning the output to Python */
 void free_data_points(int n, Point* points){
     int i;
     if (points != NULL){
@@ -29,7 +37,7 @@ void free_data_points(int n, Point* points){
     }
 }
 
-
+/* Frees clusters memory, used before returning the output to Python */
 void free_clusters(int k, Cluster* clusters){
     int j;
     for (j = 0; j < k; j++){
@@ -39,7 +47,10 @@ void free_clusters(int k, Cluster* clusters){
     free(clusters);
 }
 
-
+/* 
+Assigns the current point, given as PyList, into the given vector
+input: PyList, empty vector in which the data will be saved and dimension
+*/
 void pyList_to_array(PyObject *list, double* vector, int dim){
     PyObject *item;
     Py_ssize_t coor;
@@ -49,7 +60,11 @@ void pyList_to_array(PyObject *list, double* vector, int dim){
     }
 }
 
-
+/* 
+Allocates memory for k Clusters, and assigns each Cluster a unique centroid, zero vector and count = 0
+input: dimension and PyList of centroids
+output: list of k Clusters  
+*/
 Cluster* createClusters(int dim, PyObject *centroids, int k) {
     Py_ssize_t i;
     Cluster *clusters;
@@ -62,8 +77,8 @@ Cluster* createClusters(int dim, PyObject *centroids, int k) {
         double* newPoints = (double*) calloc(dim, sizeof(double));
         double* centroid = (double*) calloc(dim, sizeof(double));
         Mem_Assertion(newPoints != NULL && centroid != NULL);
-        item = PyList_GetItem(centroids, i);
-        pyList_to_array(item, centroid, dim);
+        item = PyList_GetItem(centroids, i); //extract the point in index i
+        pyList_to_array(item, centroid, dim); //assign the point to the new centroid
         clus.count = count;
         clus.newPoints = newPoints;
         clus.centroid = centroid;
@@ -78,6 +93,11 @@ void free_memory(int k, int n, Point* points, Cluster* clusters){
     free_clusters(k, clusters);
 }
 
+/* 
+Allocates points memory by dimension and n
+input: dimension and size
+output: an empty matrix of size n*m
+ */
 Point* allocate_mem(int dim, int n){
     int i;
     double* v;
@@ -91,6 +111,7 @@ Point* allocate_mem(int dim, int n){
     return points;
 }
 
+/* Assigns the n data points, stored in points_list, to the Points in points */
 void create_matrix(PyObject* points_list, Point* points, int dim, int n) {
     Py_ssize_t i;
     PyObject *item;
@@ -100,7 +121,11 @@ void create_matrix(PyObject* points_list, Point* points, int dim, int n) {
     }
 }
 
-
+/*
+ Computes the distance between the given point and the given centroid
+ input: point, centroid, dimension
+ output: distance, double  
+ */
 double distance(Point* point1, const double* centroid, int dim){
     int i;
     double sum, p;
@@ -112,11 +137,16 @@ double distance(Point* point1, const double* centroid, int dim){
     return sqrt(sum);
 }
 
+/*
+ Finds the closest cluster to the given point
+ input: point, list of clusters, dimension, k
+ output: index of the required cluster, int  
+*/
 int min_distance(Point* point, Cluster* clusters, int dim, int k){
     int min_index, i;
     double min_val, curr;
     min_index = 0;
-    min_val = distance(point, clusters[0].centroid, dim);
+    min_val = distance(point, clusters[0].centroid, dim); //define the distance from the first cluster as min_val
     for (i = 1; i < k; i++){
         curr = distance(point, clusters[i].centroid, dim);
         if (curr < min_val){
@@ -127,6 +157,7 @@ int min_distance(Point* point, Cluster* clusters, int dim, int k){
     return min_index;
 }
 
+/* Compute the euclidean norm */
 double euclidean_norm(const double* vector, int dim){
     int i;
     double result,p;
@@ -138,16 +169,25 @@ double euclidean_norm(const double* vector, int dim){
     return sqrt(result);
 }
 
-
+/*
+ Add the given point to the given cluster 
+ The insert operation is implemented by adding each coordinate to its adequate index in new_points vector
+ input: point, cluster, dimension
+*/
 void add_point(Point* point, Cluster* cluster, int dim){
     int i;
     for (i = 0; i < dim; i++) {
         cluster->newPoints[i] += point->vector[i];
     }
-    cluster->count += 1;
+    cluster->count += 1; //increase the number of points in the given cluster by 1 
 }
 
-
+/*
+ Update the centroid of the given cluster by computing the average value of each coordinate in new_points
+ Check convergence
+ input: cluster, dimension, an empty vector used for holding the result of the computation, eps
+ output: indicator for convergence, int
+ */
 int centroid_update(Cluster* cluster, int dim, double *tmp_vector, double eps){
     int has_changed, i, l;
     double norm_check;
@@ -170,21 +210,31 @@ int centroid_update(Cluster* cluster, int dim, double *tmp_vector, double eps){
     return has_changed;
 }
 
-
+/*
+ Update the centroids of the clusters, and initialize count and new_points for each cluster
+ Called at the end of each iteration in Kmeans
+ input: list of clusters, k, dimension, epsilon
+ output: indicator for convergence, int
+ */
 int clusters_update(Cluster* clusters, int k, int dim, double eps) {
     int changed, i, epsilon_indicator;
     double *tmp_vector;
     changed = 1;
-    tmp_vector = (double *) calloc(dim, sizeof(double));
+    tmp_vector = (double *) calloc(dim, sizeof(double)); //Create a temporary vector to hold the new values before assigning them to the centroid
     Mem_Assertion(tmp_vector != NULL);
     for (i = 0; i < k; i++) {
         epsilon_indicator = centroid_update(&clusters[i], dim, tmp_vector, eps);
         changed = ((changed) && (epsilon_indicator));
     }
-    free(tmp_vector);
+    free(tmp_vector); //free 'tmp_vector'
     return changed;
 }
 
+/*
+ array_to_Pylist transformes kmeans final centroids array of arrays to a pylists of pylists containing Python floats
+ input: clusters = list of clusters, k, dim = dimension
+ output: PyList = a python lists of lists containing final centroids points as PyFloat
+*/
 PyObject* array_to_PyList(Cluster *clusters, int k, int dim){
     Py_ssize_t i, j;
     PyObject *PyList = PyList_New((Py_ssize_t)(k));
@@ -199,10 +249,14 @@ PyObject* array_to_PyList(Cluster *clusters, int k, int dim){
     return PyList;
 }
 
+/*
+ kmeans algorithem implementaion
+ input: max_iter, n, eps, clusters = list of clusters, points = list of data points, dim = dimension, k
+*/
 void kmeans(int max_iter, int n, double eps, Cluster* clusters, Point* points, int dim, int k) {
     int epsilon_check, iter, i, index;
-    epsilon_check = 0;
-    iter = 0;
+    epsilon_check = 0; // epsilon_check is an identicator that has a value of 1 iff the euclidean norm of each centroids doesn't change by more then epsilon.
+    iter = 0; //iterations counter
     while ((iter < max_iter) && (1 - epsilon_check)) {
         for (i = 0; i < n; i++) {
             index = min_distance(&points[i], clusters, dim, k);
@@ -213,7 +267,12 @@ void kmeans(int max_iter, int n, double eps, Cluster* clusters, Point* points, i
     }
 }
 
-
+/* fit function:
+input: k, n - the number of points in data, dim - the dimension of each data point, max_iter, eps - provided epsilon, 
+centroids_list - python list of intial centroids, points_list - a python list of lists where each list is a data point in the given data
+output: raises an error if one or more of the input arguments is not in correct format,
+otherwise return final- a python list of lists with final centroids caculated by k-means algorithem implemented in kmeans.
+ */
 static PyObject* fit(PyObject *self, PyObject *args) {
     PyObject *centroids_list, *points_list, *final;
     double eps;
